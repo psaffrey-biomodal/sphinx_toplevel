dd6f02e18d0f1d3cae36b20e094086a5afc27f2b
Peter Saffrey <peter.saffrey@biomodal.com>
2025-02-20 17:25:22 +0000

    adding requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,10 +13,15 @@ author = 'biomodal Ltd.'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
 
 templates_path = ['_templates']
-exclude_patterns = []
 
 
 
@@ -24,4 +29,3 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
